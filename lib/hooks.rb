eb4acb8612c2b9c49387897cc4886eee3bae84a3
Grzegorz Bialy <grzegorz@elcodo.pl>
2013-11-24 18:03:22 +0100

    Redmine 2.3 support and Polish locale.

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -21,18 +21,20 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-class Hooks < Redmine::Hook::ViewListener
-  def controller_issues_new_before_save(context={} )
-    dest_role = Setting.plugin_autowatch['role_id'].to_i
-    issue = context[:issue]
-    project = Project.find(issue.project_id) # Get Project
-    project.members.each do |member| # Get all members from Project and loop through them
-      member.roles.each do |role|
-        if role.id == dest_role
-          # Member is in autowatch role
-          user = member.user
-          if !(issue.watched_by? user)
-            issue.add_watcher(user)
+module AutoWatch
+  class Hooks < Redmine::Hook::ViewListener
+    def controller_issues_new_before_save(context={} )
+      dest_role = Setting.plugin_autowatch['role_id'].to_i
+      issue = context[:issue]
+      project = Project.find(issue.project_id) # Get Project
+      project.members.each do |member| # Get all members from Project and loop through them
+        member.roles.each do |role|
+          if role.id == dest_role
+            # Member is in autowatch role
+            user = member.user
+            if !(issue.watched_by? user)
+              issue.add_watcher(user)
+            end
           end
         end
       end
